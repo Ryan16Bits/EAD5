c077f08950bf5101d0eceb9395a4eced7f404ff9
Ryan <0000727606@senaimgaluno.com.br>
2025-11-20 00:15:10 -0300

    Mudança na classe Main

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,20 @@ public class Main {
 
         Lutador vc = null;
         Lutador op = null;
-        int numeroRandom = random.nextInt(1,4);
+        int numeroRandom = random.nextInt(3) + 1;
 
         System.out.println("Insira seu nome: ");
         String nome = sc.nextLine();
+
+        System.out.println("---------------");
 
         System.out.println("Escolha sua raça: ");
         System.out.println("1: Orc - Mais forte, mais vida, magia fraca");
         System.out.println("2: Imperial - Status medianos");
         System.out.println("3: Khajiit - Mais fracos, menos vida, magia forte");
         int opcao = sc.nextInt();
+
+        System.out.println("---------------");
 
         switch (opcao) {
             case 1: {
@@ -37,22 +41,34 @@ public class Main {
 
         switch (numeroRandom) {
             case 1: {
-                op = new Orc(nome);
+                op = new Orc("Orc");
                 break;
             }
 
             case 2: {
-                op = new Imperial(nome);
+                op = new Imperial("Imperial");
                 break;
             }
 
             case 3: {
-                op = new Khajiit(nome);
+                op = new Khajiit("Khajiit");
             }
         }
 
-        System.out.println(vc);
+        System.out.println("Ao andar pelas florestas de Skyrim, você se depara com um " + op.getNome() + " bandido.");
+        System.out.println();
 
-        System.out.println(op);
+        System.out.println(op.getNome() + ": Entregue seus objetos de valor ou eu te esquartejo como um peixe.");
+        System.out.println("---------------");
+
+        System.out.println("Você: ");
+        vc.mostrarStatus();
+        System.out.println("---------------");
+        System.out.println("Inimigo: ");
+        op.mostrarStatus();
+        System.out.println("---------------");
+
+        System.out.println("O que deseja fazer?");
+
     }
 }
